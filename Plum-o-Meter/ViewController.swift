212da7864418293e7ed36229afd0fdb25ceb49b9
Simon Gladman <sgladman@gmail.com>
2015-10-24 14:01:31 +0100

    Initial Commit

---
+++ Plum-o-Meter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Plum-o-Meter
+//
+//  Created by Simon Gladman on 24/10/2015.
+//  Copyright © 2015 Simon Gladman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
